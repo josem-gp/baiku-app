c59ff3a113f7ae1531710a097c676ce5c258036d
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-05-31 18:20:53 +0900

    cloudinary set up done

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -3,6 +3,7 @@ class Parking < ApplicationRecord
   has_many :user_parkings
   has_many :users, through: :user_parkings
   has_many :reviews, dependency: :destroy
+  has_many_attached :photos
   validates :name, uniqueness: { scope: :address }, presence: true
   validates :address, presence: true
   validates :description, length: { minimum: 10 }, presence: true
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :parking
   has_many :replies, dependency: :destroy
+  has_one_attached :photo
   validates :comment, presence: true
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
