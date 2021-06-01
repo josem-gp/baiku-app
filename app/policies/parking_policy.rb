e38a3d8a70a8957aeff182337616065ada897fbe
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-06-01 15:06:21 +0900

    working on parking view

--- app/policies/parking_policy.rb
+++ app/policies/parking_policy.rb
@@ -9,4 +9,19 @@ class ParkingPolicy < ApplicationPolicy
     true
   end
   
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
 end
