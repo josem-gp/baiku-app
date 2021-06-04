5052d10005f4f0e0e864870cfe03d22bc33145d4
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-04 15:11:58 +0900

    seeds

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,19 +1,19 @@
 class Parking < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  
+
   has_many :user_parkings
   has_many :users, through: :user_parkings
   has_many :reviews, dependent: :destroy
   has_many_attached :photos
   validates :name, uniqueness: { scope: :address }, presence: true
   validates :address, presence: true
-  validates :description, length: { minimum: 10 }, presence: true
+  # validates :description, length: { minimum: 10 }, presence: true
   enum price: [:free, :paid]
 
   include PgSearch::Model
   pg_search_scope :search_by_name_and_address,
-  against: [ :name, :address ],
+    against: [ :name, :address ],
   using: {
     tsearch: { prefix: true }
   }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,82 +1,106 @@
 require "open-uri"
+require 'nokogiri'
 
-puts "Clearing database..."
-Parking.destroy_all
-Review.destroy_all
-User.destroy_all
+# puts "Clearing database..."
+# Parking.destroy_all
+# Review.destroy_all
+# User.destroy_all
 
-name = ['Impact HUB Tokyo', 'Maruetsu Meguro', 'Riverside Earth Training Center', 'Switch Coffee Tokyo',
-        'JR Tokyumeguro Bldg', 'Otori-jinja Shrine', 'Meguro Tokyu Store', 'Family Mart Otori-jinja',
-        '7 Eleven Meguro Gyoninzaka', 'Pine Heights Meguro', '7 Eleven Meguro Central Suqare', 'Prime Urban Meguro',
-        'Cocokara Fine Meguro', 'Image Studio 109 Meguro Studio', 'Family Mart Meguro East', 'Voice Training Academy Meguro']
+# name = ['Impact HUB Tokyo', 'Maruetsu Meguro', 'Riverside Earth Training Center', 'Switch Coffee Tokyo',
+#         'JR Tokyumeguro Bldg', 'Otori-jinja Shrine', 'Meguro Tokyu Store', 'Family Mart Otori-jinja',
+#         '7 Eleven Meguro Gyoninzaka', 'Pine Heights Meguro', '7 Eleven Meguro Central Suqare', 'Prime Urban Meguro',
+#         'Cocokara Fine Meguro', 'Image Studio 109 Meguro Studio', 'Family Mart Meguro East', 'Voice Training Academy Meguro', 'Seven-Eleven Minatoku Tameikesanno store',
+#         'A1-01.千代田区役所 / Chiyoda City Office', 'Tokyo Kusei Kaikan building(Metro A5 Exit)', 'Nishi-Shinbashi 1Chome (Sotobori St.)', 'TORANOMON HILLS',
+#         'Nishi-Shinbashi 2-Chome (Shintora St.)', 'Atago GREEN HILLS']
 
-address = ['2-11-3 Meguro, Tokyo', '2-21-23 Meguro, Tokyo', '2-chōme-1-19 Shimomeguro', '1-17-23 Meguro, Tokyo', '3-1 Shinagawa, Tokyo',
-           '3-1-23 Meguro, Tokyo', '2-24-13 Shinagawa, Tokyo', '3-10 Meguro, Tokyo', '1-7-8 Meguro, Tokyo', '2-11-3 Meguro, Tokyo', '3-1-1 Shinagawa, Tokyo',
-           '1-11-11 Meguro, Tokyo', '1-6-17 Meguro, Tokyo', '2-24-12 Shimomeguro, Tokyo', '2-12-2 Meguro, Tokyo', '2-18-16 Shinagawa, Tokyo']
+# address = ['2-11-3 Meguro, Tokyo', '2-21-23 Meguro, Tokyo', '2-chōme-1-19 Shimomeguro', '1-17-23 Meguro, Tokyo', '3-1 Shinagawa, Tokyo',
+#            '3-1-23 Meguro, Tokyo', '2-24-13 Shinagawa, Tokyo', '3-10 Meguro, Tokyo', '1-7-8 Meguro, Tokyo', '2-11-3 Meguro, Tokyo', '3-1-1 Shinagawa, Tokyo',
+#            '1-11-11 Meguro, Tokyo', '1-6-17 Meguro, Tokyo', '2-24-12 Shimomeguro, Tokyo', '2-12-2 Meguro, Tokyo', '2-18-16 Shinagawa, Tokyo', '港区赤坂1-4-41',
+#            '千代田区九段南1-2-1', '千代田区飯田橋3-5-1', '港区西新橋1-12-10', '港区虎ノ門1-23-1', '港区西新橋2-22-7', '港区愛宕2-5-1']
 
-description = ['Free for members', 'Free for customers', 'Quiet area near the a training center', 'Small street. Cafe and river nearby', 'Lots of parking spaces',
-               'Next to Otori-jinja', 'Free for customers', 'Quiet street behind Family Mart. Free for customers', 'Free for customers', "Quiet residential area. Mainly seniors that don't say much",
-               'Free for customers', 'Small street near Mita Okanoue Park', 'Free for customers', 'Opposite a photography studio', 'Free for customers', 'Next to an academy. Plenty of space']
+# description = ['Free for members', 'Free for customers', 'Quiet area near the a training center', 'Small street. Cafe and river nearby', 'Lots of parking spaces',
+#                'Next to Otori-jinja', 'Free for customers', 'Quiet street behind Family Mart. Free for customers', 'Free for customers', "Quiet residential area. Mainly seniors that don't say much",
+#                'Free for customers', 'Small street near Mita Okanoue Park', 'Free for customers', 'Opposite a photography studio', 'Free for customers', 'Next to an academy. Plenty of space']
 
-images = ['https://res.cloudinary.com/dqjzulqyf/image/upload/v1622449850/Baiku/impactHub_pq4jb2.png', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622449850/Baiku/maruetsu_meguro_plqefa.jpg',
-          'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451058/Baiku/riverside_m5fsig.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622452484/Baiku/SWITCH_COFFEE_SHOP_fhqw3n.jpg',
-          'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622450476/Baiku/jr_megurobldg_uz6gbe.png', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451919/Baiku/otorikjinja_fmlvjb.png',
-          'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451063/Baiku/tokyu_store_vgrfjh.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451800/Baiku/family_mart_otori_dyn6kd.jpg',
-          'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451064/Baiku/7_eleven_gyoninzaka_ynhhyo.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451946/Baiku/pine_heights_oaws8m.jpg',
-          'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451066/Baiku/seven_eleven_central_square_bxxztf.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622450705/Baiku/Prime_urban_meguro_t1oxmf.jpg',
-          'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622450773/Baiku/cocokara_fine_byiw0u.png', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451828/Baiku/Image_Studio_109_Meguro_studio_wnnn7n.jpg',
-          'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622450373/Baiku/familymart_meguroeast_axm4jc.png', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451077/Baiku/voice_academy_et6sie.png']
+# images = ['https://res.cloudinary.com/dqjzulqyf/image/upload/v1622449850/Baiku/impactHub_pq4jb2.png', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622449850/Baiku/maruetsu_meguro_plqefa.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451058/Baiku/riverside_m5fsig.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622452484/Baiku/SWITCH_COFFEE_SHOP_fhqw3n.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622450476/Baiku/jr_megurobldg_uz6gbe.png', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451919/Baiku/otorikjinja_fmlvjb.png',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451063/Baiku/tokyu_store_vgrfjh.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451800/Baiku/family_mart_otori_dyn6kd.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451064/Baiku/7_eleven_gyoninzaka_ynhhyo.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451946/Baiku/pine_heights_oaws8m.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451066/Baiku/seven_eleven_central_square_bxxztf.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622450705/Baiku/Prime_urban_meguro_t1oxmf.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622450773/Baiku/cocokara_fine_byiw0u.png', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451828/Baiku/Image_Studio_109_Meguro_studio_wnnn7n.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622450373/Baiku/familymart_meguroeast_axm4jc.png', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622451077/Baiku/voice_academy_et6sie.png',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622781403/Baiku/5AF2TALq2gSTShyMDRIbqnFmrSGKJ1vD3xDwepKdg13UwSpBP8UTaJS2Y6xsSREErWypgWWau6z-aWYiuiE6_R9XNpnKbs0KJBv1c3Q8nDN3hZwJ5b6UFHnlSt68n-W1zGP6Zbyf4gqlKYBN-0PPjQsHYY8q7ENgeFi5No6XZqlDXGDQm9SmPZbrejI51gMCU2Ie2jVWsEj7q_qm0bwz.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622782006/Baiku/5AF2TALrBestEAkYFuSIL3LIJFsJa9s1_CwRjAiSfIk7hi6A7n1oO_0saaScIolfadMjU7v85im_N1kNme21luuJr-kVpYx7tP-Z_FjK9e30_MS297KnpzNxqA2Ry-qTCMHptVP_6tDsH-qyakhnuCMHHFrUA4ickY3ilUqqMokVr8VJYSx5Kp42aHsdEel28hLjIHmKVvYd5_inlirf.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622782080/Baiku/3_reggef.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622784996/Baiku/4_pqlx6l.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622785076/Baiku/5_bwxopg.jpg', 'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622785146/Baiku/6_kw35sn.jpg',
+#           'https://res.cloudinary.com/dqjzulqyf/image/upload/v1622785209/Baiku/7_dgvq8u.jpg']
 
-puts "Creating parking"
+# puts "Creating parking"
 
-name.each_with_index do |nam,idx|
+# name.each_with_index do |nam,idx|
 
-  p images[idx]
-  file = URI.open(images[idx])
-  parking = Parking.new(name: nam, address: address[idx], description: description[idx], price: rand(0..1))
-  parking.save!
-  parking.photos.attach(io: file, filename: 'parking.png', content_type: 'image/jpg')
-  puts "Parking #{idx + 1} created!"
+#   p images[idx]
+#   file = URI.open(images[idx])
+#   parking = Parking.new(name: nam, address: address[idx], description: description[idx], price: rand(0..1))
+#   parking.save!
+#   parking.photos.attach(io: file, filename: 'parking.png', content_type: 'image/jpg')
+#   puts "Parking #{idx + 1} created!"
 
+# end
+
+# puts "Created #{Parking.count} parkings!"
+
+# puts "Creating some users..."
+
+# user = User.new(email: 'lenatheboss@hotmail.com', password: '1234567', name: 'Lena')
+# user.save!
+
+# user = User.new(email: 'jose@hotmail.com', password: '1234567', name: 'Jose')
+# user.save!
+
+# user = User.new(email: 'allan@hotmail.com', password: '1234567', name: 'Allan')
+# user.save!
+
+# user = User.new(email: 'kenn@hotmail.com', password: '1234567', name: 'Kenn')
+# user.save!
+
+# user = User.new(email: 'doug@hotmail.com', password: '1234567', name: 'Doug')
+# user.save!
+
+# puts "Created #{User.count} users!"
+
+# puts "Creating some reviews..."
+
+# review_5 = Review.new(comment: "Perfect! Very quiet and had no issues. Will definitely use this spot again.", rating: 5)
+# review_4 = Review.new(comment: "Great place! I've used this spot twice now. Quite a few bikes, but I", rating: 4)
+# review_3 = Review.new(comment: "Good. No problems this time. The street is quite busy though. I might try and look for a quieter place.", rating: 3)
+# review_2 = Review.new(comment: "I could park my bike here, but one of the locals living across the road warned me not to.", rating: 2)
+# review_1 = Review.new(comment: "Terrible place! Someone stole my bicycle seat. WTF? NOT recommended.", rating: 1)
+# reviews = [review_1, review_2, review_3, review_4, review_5]
+
+# 20.times do
+#   review = reviews.sample.dup
+#   review.user = User.all.sample
+#   review.parking = Parking.all.sample
+#   review.save!
+# end
+
+
+# puts "Created #{Review.count} reviews!"
+# puts "Done!"
+
+puts "Start the Scraping"
+
+url = "xmlSeed/seedBikes.kml"
+
+puts "url checked!"
+file = File.open(url)
+# html_file = URI.open(url).read
+document  = Nokogiri::XML(file)
+
+puts "html_doc checked!"
+
+p document.search('coordinates').each do |coordinates|
+  p coordinates
 end
-
-puts "Created #{Parking.count} parkings!"
-
-puts "Creating some users..."
-
-user = User.new(email: 'lenatheboss@hotmail.com', password: '1234567', name: 'Lena')
-user.save!
-
-user = User.new(email: 'jose@hotmail.com', password: '1234567', name: 'Jose')
-user.save!
-
-user = User.new(email: 'allan@hotmail.com', password: '1234567', name: 'Allan')
-user.save!
-
-user = User.new(email: 'kenn@hotmail.com', password: '1234567', name: 'Kenn')
-user.save!
-
-user = User.new(email: 'doug@hotmail.com', password: '1234567', name: 'Doug')
-user.save!
-
-puts "Created #{User.count} users!"
-
-puts "Creating some reviews..."
-
-review_5 = Review.new(comment: "Perfect! Very quiet and had no issues. Will definitely use this spot again.", rating: 5)
-review_4 = Review.new(comment: "Great place! I've used this spot twice now. Quite a few bikes, but I", rating: 4)
-review_3 = Review.new(comment: "Good. No problems this time. The street is quite busy though. I might try and look for a quieter place.", rating: 3)
-review_2 = Review.new(comment: "I could park my bike here, but one of the locals living across the road warned me not to.", rating: 2)
-review_1 = Review.new(comment: "Terrible place! Someone stole my bicycle seat. WTF? NOT recommended.", rating: 1)
-reviews = [review_1, review_2, review_3, review_4, review_5]
-
-20.times do
-  review = reviews.sample.dup
-  review.user = User.all.sample
-  review.parking = Parking.all.sample
-  review.save!
-end
-
-
-puts "Created #{Review.count} reviews!"
-puts "Done!"
