1a66dfe1c607da1f66a7b6865b9caac742dd1c9a
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-06-04 12:00:42 +0900

    Changed naming of the icons, width & height of the card image

--- app/javascript/plugins/init_carousel.js
+++ app/javascript/plugins/init_carousel.js
@@ -2,28 +2,28 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 
 const initCarousel = () => {
-  const owl = $('.owl-carousel')
+  const owl = $('.owl-carousel');
   owl.owlCarousel({
-    // stagePadding: 8,
+    // stagePadding: 2,
     loop: false,
     margin: 10,
-    responsive:{
-      0:{
-        items:1
-       },
-      600:{
-       items:3
-       },
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
       1000: {
-          items: 5
-      }
-     },
-   })
+        items: 5,
+      },
+    },
+  });
   owl.on('changed.owl.carousel', function(event) {
     const card = document.querySelectorAll('.card')[event.item.index]
     document.querySelectorAll('.animate').forEach((marker) => marker.classList.remove('animate'));
     document.getElementById(`marker-studio-${card.dataset.id}`).classList.add("animate");
   })
-}
+};
 
 export { initCarousel };
