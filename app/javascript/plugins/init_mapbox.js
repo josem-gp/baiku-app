644fd319eb75071e0a2dc49e0995ac3bee6d7eb7
Lena Namiki <lena.namiki@gmail.com>
2021-06-07 18:06:15 +0900

    Created function to locate user upon map load

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,25 +1,6 @@
 import mapboxgl from 'mapbox-gl';
-// import 'mapbox-gl/dist/mapbox-gl.css';
 import distance from '@turf/distance';
 import { initRoute } from './init_route';
-
-// const geolocateStart = (map) => {
-//   var canvas = map.getCanvasContainer();
-//   // initialize a start corrdinate
-//   let start = [];
-
-//   // Add geolocate control to the map.
-//   let geolocate = new mapboxgl.GeolocateControl({
-//     positionOptions: {
-//       enableHighAccuracy: true
-//     },
-//     trackUserLocation: true
-//   });
-
-//   // Add geolocate control button to the map.
-//   map.addControl(geolocate);
-//   return geolocate
-// }
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -40,16 +21,21 @@ const initMapbox = () => {
       zoom: 12
     });
 
-    // const geolocate = geolocateStart(map);
-    // // Geolocate your position and initialize the route rendering
-    // geolocate.on('geolocate', function (position) {
-    //   const latitude = position.coords.latitude;
-    //   const longitude = position.coords.longitude;
-    //   const start = [longitude, latitude];
-      if (mapElement.dataset.destination) {
-        initRoute(map);
-      }
-    // });
+    const geolocate = map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+        trackUserLocation: true
+    }));
+    // Add the control to the map.
+    map.addControl(geolocate);
+    map.on('load', function() {
+      document.querySelector('.mapboxgl-ctrl-geolocate').click();
+    });
+
+  if (mapElement.dataset.destination) {
+    initRoute(map);
+  }
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
