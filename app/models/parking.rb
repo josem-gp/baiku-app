29cb9dcc72dfd33adaea6857e5c51f308307b5ae
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-07 18:57:47 +0900

    Solved bug create location

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,4 +1,7 @@
 class Parking < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode, if: :will_save_change_to_latitude?
 
