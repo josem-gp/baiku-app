ddedc9af4932b896ebc8fd97e529386d43209eda
Lena Namiki <lena.namiki@gmail.com>
2021-06-09 12:34:09 +0900

    Added validation to notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :parking
+  validates :parking_id, presence: true
   validates :message, presence: true
   enum message: [:risk, :new_parking]
 end
