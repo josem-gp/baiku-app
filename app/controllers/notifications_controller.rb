77be2c2ed89cf78af294ff434aceae062cd0549d
Lena Namiki <lena.namiki@gmail.com>
2021-06-10 11:52:54 +0900

    Styled page and made cards clickable to a parking spot

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,16 @@ class NotificationsController < ApplicationController
   def index
     @notifications = policy_scope(Notification)
     @notifications.update_all(read: true)
+
+    @notifications.each do |notification|
+      today_date = (Date.today).strftime('%b %dth, %Y')
+      creation_date = (notification.created_at).strftime('%b %dth, %Y')
+      if today_date == creation_date
+        @date = 'Today'
+      else
+        @date = creation_date
+      end
+    end
+    @date
   end
 end
