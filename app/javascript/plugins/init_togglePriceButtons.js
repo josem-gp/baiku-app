7f2875f07d811364983c124dc515cf9d462a4fcc
Lena Namiki <lena.namiki@gmail.com>
2021-06-10 17:23:40 +0900

    Uncommented broken js for now

---
+++ app/javascript/plugins/init_togglePriceButtons.js
@@ -0,0 +1,25 @@
+// const togglePriceButtons = () => {
+//   const buttons = document.querySelectorAll('.form-check');
+//   const activeBtns = document.querySelectorAll('.btn-active');
+
+//   buttons.forEach((btn) => {
+//     btn.addEventListener('click', () => {
+//       // if (btn.classList.contains('btn-active')) {
+//         activeBtns.forEach((activeBtn) => {
+//           activeBtn.classList.remove('btn-active');
+//         })
+//       event.currentTarget.classList.add('btn-active');
+//     })
+//   })
+//   // const activeBtns = document.querySelectorAll("btn-active")
+
+//   // clickedBtn.addEventListener("click", () => {
+//   //   activeBtns.forEach( (activeBtn) => {
+//   //     activeBtn.classList.remove("active");
+//   //   });
+//   //   clickedBtn.classList.add("active");
+//   // });
+// };
+
+
+// export { togglePriceButtons };
