6f4e87b8914cdb5359ba38eefd7b4c4293a74bc8
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-06-11 16:34:13 +0900

    added about page to profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
 
   resources :parkings, except: [:edit, :update, :destroy] do
     resources :reviews, only: :create
