7310ff2b2a043c86e22ed57439a4c8fc362157b0
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-09-14 20:52:27 +0900

    Gave the carousel a makeover

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,7 +21,7 @@ const initMapbox = () => {
     });
 
     // This makes the route stay within the viewport
-    map.setPadding({top: 40, bottom: 150}); 
+    map.setPadding({top: 40, bottom: 160}); 
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
